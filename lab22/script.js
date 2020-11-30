674d025761a76cc8854cd24e885bdf6d44bbfb46
an-rabtsevich <60143623+an-rabtsevich@users.noreply.github.com>
2020-11-30 22:57:05 +0300

    qqq

---
+++ lab22/script.js
@@ -0,0 +1,98 @@
+var nVer = navigator.appVersion;
+var nAgt = navigator.userAgent;
+var browserName  = navigator.appName;
+var fullVersion  = ''+parseFloat(navigator.appVersion); 
+var majorVersion = parseInt(navigator.appVersion,10);
+var nameOffset,verOffset,ix;
+
+// In Opera, the true version is after "Opera" or after "Version"
+if ((verOffset=nAgt.indexOf("Opera"))!=-1) {
+ browserName = "Opera";
+ fullVersion = nAgt.substring(verOffset+6);
+ if ((verOffset=nAgt.indexOf("Version"))!=-1) 
+   fullVersion = nAgt.substring(verOffset+8);
+}
+// In MSIE, the true version is after "MSIE" in userAgent
+else if ((verOffset=nAgt.indexOf("MSIE"))!=-1) {
+ browserName = "Microsoft Internet Explorer";
+ fullVersion = nAgt.substring(verOffset+5);
+}
+// In Chrome, the true version is after "Chrome" 
+else if ((verOffset=nAgt.indexOf("Chrome"))!=-1) {
+ browserName = "Chrome";
+ fullVersion = nAgt.substring(verOffset+7);
+}
+// In Safari, the true version is after "Safari" or after "Version" 
+else if ((verOffset=nAgt.indexOf("Safari"))!=-1) {
+ browserName = "Safari";
+ fullVersion = nAgt.substring(verOffset+7);
+ if ((verOffset=nAgt.indexOf("Version"))!=-1) 
+   fullVersion = nAgt.substring(verOffset+8);
+}
+// In Firefox, the true version is after "Firefox" 
+else if ((verOffset=nAgt.indexOf("Firefox"))!=-1) {
+ browserName = "Firefox";
+ fullVersion = nAgt.substring(verOffset+8);
+}
+// In most other browsers, "name/version" is at the end of userAgent 
+else if ( (nameOffset=nAgt.lastIndexOf(' ')+1) < 
+          (verOffset=nAgt.lastIndexOf('/')) ) 
+{
+ browserName = nAgt.substring(nameOffset,verOffset);
+ fullVersion = nAgt.substring(verOffset+1);
+ if (browserName.toLowerCase()==browserName.toUpperCase()) {
+  browserName = navigator.appName;
+ }
+}
+// trim the fullVersion string at semicolon/space if present
+if ((ix=fullVersion.indexOf(";"))!=-1)
+   fullVersion=fullVersion.substring(0,ix);
+if ((ix=fullVersion.indexOf(" "))!=-1)
+   fullVersion=fullVersion.substring(0,ix);
+
+majorVersion = parseInt(''+fullVersion,10);
+if (isNaN(majorVersion)) {
+ fullVersion  = ''+parseFloat(navigator.appVersion); 
+ majorVersion = parseInt(navigator.appVersion,10);
+}
+
+alert("Browser name: " + browserName + "\n" + 'Full version: '+ fullVersion)
+
+////////////////////////////////////////////////////////////////////////////
+
+window.setInterval(ut, 1000);
+
+function ut() 
+{
+  var d = new Date();
+  document.getElementById("time").innerHTML = d.toLocaleTimeString();
+  document.getElementById("date").innerHTML = d.toLocaleDateString();
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+var counter = 6;
+var i;
+function timer ()
+{
+    counter--;
+    document.getElementById("timer").innerHTML = counter;
+    start();
+}
+
+function start() 
+{
+    document.getElementById("timer").style.display = 'block';
+    document.getElementById("timerText").style.display = 'block';
+    var id = setTimeout("timer()", 1000);
+    if(counter == 0)
+    {
+        clearInterval(id);
+        counter = 6;
+        document.getElementById("timer").style.display = 'none';
+        document.getElementById("timerText").style.display = 'none';
+        document.getElementById("clap").style.display = 'block';
+        document.getElementById("div1").style.display = 'none';
+        document.getElementById("but").style.display = 'none';
+    }
+}
