d8806d38cac84134ee8de68f9f8f1b03c317a1fd
an-rabtsevich <60143623+an-rabtsevich@users.noreply.github.com>
2020-12-01 13:29:48 +0300

    qqq

---
+++ lab24/script.js
@@ -0,0 +1,6 @@
+function start()
+{
+    document.getElementById("everything").style.display = 'none';
+    document.getElementById("NY").style.display = 'block';
+    document.body.style.background = 'black';
+}
