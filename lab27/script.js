285cf0c011c8fa9debbb5f34184fe486c36b819f
an-rabtsevich <60143623+an-rabtsevich@users.noreply.github.com>
2020-12-12 01:43:19 +0300

    qqq

---
+++ lab27/script.js
@@ -0,0 +1,55 @@
+function film1()
+{
+    document.getElementById("film1").style.display = "block";
+    document.getElementById("film2").style.display = "none";
+    document.getElementById("film3").style.display = "none";
+    document.getElementById("film4").style.display = "none";
+}
+function film2()
+{
+    document.getElementById("film1").style.display = "none";
+    document.getElementById("film2").style.display = "block";
+    document.getElementById("film3").style.display = "none";
+    document.getElementById("film4").style.display = "none";
+}
+function film3()
+{
+    document.getElementById("film1").style.display = "none";
+    document.getElementById("film2").style.display = "none";
+    document.getElementById("film3").style.display = "block";
+    document.getElementById("film4").style.display = "none";
+}
+function film4()
+{
+    document.getElementById("film1").style.display = "none";
+    document.getElementById("film2").style.display = "none";
+    document.getElementById("film3").style.display = "none";
+    document.getElementById("film4").style.display = "block";
+}
+
+
+function sled1()
+{
+    document.getElementById("film1").style.display = "none";
+    document.getElementById("film2").style.display = "block";
+    document.getElementById("film3").style.display = "none";
+    document.getElementById("film4").style.display = "none";
+}function sled2()
+{
+    document.getElementById("film1").style.display = "none";
+    document.getElementById("film2").style.display = "none";
+    document.getElementById("film3").style.display = "block";
+    document.getElementById("film4").style.display = "none";
+}function sled3()
+{
+    document.getElementById("film1").style.display = "none";
+    document.getElementById("film2").style.display = "none";
+    document.getElementById("film3").style.display = "none";
+    document.getElementById("film4").style.display = "block";
+}function sled4()
+{
+    document.getElementById("film1").style.display = "block";
+    document.getElementById("film2").style.display = "none";
+    document.getElementById("film3").style.display = "none";
+    document.getElementById("film4").style.display = "none";
+}
